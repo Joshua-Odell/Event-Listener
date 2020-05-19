872939fd17165802d17fc3a4fe9d41395322a6ae
Joshua Odell <joshodell220@gmail.com>
2020-05-19 17:58:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,23 +11,22 @@ function run (){
 //This function listens to add item button then creates a new li with the same format
 // 
 function addMechanic (){
-    $('#js-shopping-list-form button').click(function(event){
+    $('#js-shopping-list-form button').click(function(event){ 
         event.preventDefault();
+        console.log("one");
         let additem = $('#shopping-list-entry').val();      
         $('.shopping-list').append(
-            '<li>' +
-            '<span class="shopping-item">' + 
-            additem +
-            '</span>' +
-            '<div class="shopping-item-controls">' +
-          '<button class="shopping-item-toggle">' +
-            '<span class="button-label">check</span>' +
-          '</button>' +
-          '<button class="shopping-item-delete">' +
-            '<span class="button-label">delete</span>' +
-          '</button>' +
-            '</div>' +
-            '</li>');
+            `<li>
+                <span class="shopping-item">${additem}</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle ${additem}">
+                        <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                        <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`);
         ;
         
     }
@@ -37,7 +36,7 @@ function addMechanic (){
 //  when shopping-item-toggle is pressed this function adds  shopping-item__checked class to span class
 // This Works
 function checkMechanic (){
-    $('.shopping-item-toggle').on("click", function(event) {
+    $(document).on("click", '.shopping-item-toggle', function(event) {
         $(this).closest('li').find(".shopping-item").toggleClass('shopping-item__checked');
     }    
 )};
@@ -45,24 +44,12 @@ function checkMechanic (){
 // when shopping-item-delete is pressed
 // If i used the coutner it would just delete the latest item 
 function deleteMechanic (){
-    $('.shopping-item-delete').on("click", function(event) {
-        console.log($(this).closest);
-        $(this).closest('ul').remove();
+    $(document).on("click", '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove();
     }
 )};
 
 
-
-
-
-
-
-
-
-
-
-
-
 $(run);
 
 
@@ -72,4 +59,3 @@ $(run);
 
 
 
-$(run);
